2e23600bcc60b676781c89a0d8de4f62e25d4b91
dolokelen <dolokelen1@gmail.com>
2024-03-28 12:13:13 +0800

    Installed and configured Chakra UI

---
+++ src/accounts/RegistrationForm.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@chakra-ui/react";
+
+const RegistrationForm = () => {
+  return <Button colorScheme="blue">This is a button</Button>;
+};
+
+export default RegistrationForm;
