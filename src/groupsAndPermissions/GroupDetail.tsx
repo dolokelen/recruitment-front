876add0db219f81244686642da748a6aaa14447a
dolokelen <dolokelen1@gmail.com>
2024-04-04 09:00:58 +0800

    Refactoring: Group detail page.

--- src/groupsAndPermissions/GroupDetail.tsx
+++ src/groupsAndPermissions/GroupDetail.tsx
@@ -6,11 +6,12 @@ import {
   Grid,
   GridItem,
   HStack,
+  Heading,
   List,
   ListItem,
   Text,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 
@@ -62,14 +63,17 @@ const GroupDetailPage = () => {
 
   return (
     <>
-      <GroupEditForm />
+      <Box bg='green.700'>
+        <GroupEditForm />
+        </Box>
 
-      <HStack fontSize={30} mt={8} mb={3} justifyContent="space-evenly">
+      {/* <HStack fontSize={30} mt={8} mb={3} justifyContent="space-evenly">
         <Box ml="-10%">{group?.name} Permissions</Box>
         <Box>Available Permissions</Box>
       </HStack>
 
       <Grid
+        bg="gold"
         templateAreas={{
           base: `"groupPermissions unAssignPermissions" "buttons buttons"`,
           //   sm: `"nav nav" "aside main"`,
@@ -133,7 +137,99 @@ const GroupDetailPage = () => {
             </Button>
           </Flex>
         </GridItem>
-      </Grid>
+      </Grid> */}
+
+      <Box
+        my={8}
+        px={4}
+        bg="skyblue"
+        display={{ md: "flex" }}
+        justifyContent="space-between"
+      >
+        <Box bg="orangered">
+          <Heading
+            textAlign={{ base: "center" }}
+            fontSize={{ base: "1rem", md: "1.5rem" }}
+          >
+            Group Permissions
+          </Heading>
+          <OverflowYContainer>
+            <List>
+              {group?.permissions?.length ? (
+                group.permissions?.map((p) => (
+                  <ListItem key={p.id}>
+                    <Checkbox
+                      isChecked={selectedPermissions.includes(p.id)}
+                      onChange={() => handleCheckboxChange(p.id)}
+                    >
+                      {p.name}
+                    </Checkbox>
+                  </ListItem>
+                ))
+              ) : (
+                <ListItem color={red}>No assigned permissions</ListItem>
+              )}
+            </List>
+          </OverflowYContainer>
+          {selectedPermissions.length === 0 ? (
+            <Button
+              mt={4}
+              w={{ base: "90%", sm: "40%", md: "90%" }}
+              mx={2}
+              // transform='translateX(50%)'
+              isActive
+              isDisabled
+              colorScheme={blue}
+            >
+              Remove
+            </Button>
+          ) : (
+            <Button
+              // transform='translateX(50%)'
+              mt={4}
+              w={{ base: "90%", sm: "40%", md: "90%" }}
+              mx={2}
+              isActive
+              onClick={handlePermissionsRemoval}
+              colorScheme={blue}
+            >
+              Remove
+            </Button>
+          )}
+        </Box>
+
+        <Button
+          isActive
+          colorScheme="red"
+          display={{ base: "none", md: "inline-block" }}
+        >
+          Delete Group1
+        </Button>
+
+        <Box bg="teal">
+          <Heading
+            textAlign={{ base: "center" }}
+            fontSize={{ base: "1rem", md: "1.5rem" }}
+            marginTop={{ base: "2rem" }}
+          >
+            Available Permissions
+          </Heading>
+          <PermissionList assignPermissions={group?.permissions} />
+          {/* <Button transform='translateX(50%)' isActive isDisabled colorScheme={blue}>
+            Add
+          </Button> */}
+        </Box>
+
+        <Button 
+        isActive 
+        colorScheme="yellow" 
+        mt={{base: 6}} 
+        display={{ md: "none" }}
+        w={{ base: "90%", sm: "40%", md: "90%" }}
+        >
+          Delete Group2
+        </Button>
+      </Box>
     </>
   );
   // return (
--- src/groupsAndPermissions/PermissionList.tsx
+++ src/groupsAndPermissions/PermissionList.tsx
@@ -1,76 +1,87 @@
-import {
-    Button,
-    Checkbox,
-    List,
-    ListItem,
-    Spinner
-  } from "@chakra-ui/react";
-  import { useState } from "react";
-  import { useParams } from "react-router-dom";
-  import { toast } from "react-toastify";
-  import { useAddGroupPermissions } from "../hooks/useGroups";
-  import { Permission, usePermissions } from "../hooks/usePermissions";
-  import OverflowYContainer from "./OverflowYContainer";
+import { Button, Checkbox, List, ListItem, Spinner } from "@chakra-ui/react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useAddGroupPermissions } from "../hooks/useGroups";
+import { Permission, usePermissions } from "../hooks/usePermissions";
+import OverflowYContainer from "./OverflowYContainer";
 import { blue } from "../colors";
-  
-  interface Props {
-    assignPermissions?: Permission[];
-  }
-  
-  const PermissionList = ({ assignPermissions }: Props) => {
-    const { data: allPermissions, isLoading, error } = usePermissions();
-    const [selectedPermissions, setSelectedPermissions] = useState<number[]>([]);
-  
-    const { id } = useParams();
-    const groupId = parseInt(id!);
-  
-    const handleAddPermissions = useAddGroupPermissions(
-      { id: groupId, permission_ids_to_add: selectedPermissions },
-      () => {
-        setSelectedPermissions([]);
-        toast.success("Permissions removed successfully!");
-      }
-    );
-  
-    if (error) throw error;
-    if (isLoading) return <Spinner />;
-  
-    const handleCheckboxChange = (permissionId: number) => {
-      if (selectedPermissions.includes(permissionId)) {
-        setSelectedPermissions(
-          selectedPermissions.filter((id) => id !== permissionId)
-        );
-      } else {
-        setSelectedPermissions([...selectedPermissions, permissionId]);
-      }
-    };
-  
-    return (
-      <OverflowYContainer>
-        <List>
-          {allPermissions
-            ?.filter((p) => !assignPermissions?.some((ap) => ap.id === p.id))
-            .map((p) => (
-              <ListItem key={p.id}>
-                <Checkbox
-                  isChecked={selectedPermissions.includes(p.id)}
-                  onChange={() => handleCheckboxChange(p.id)}
-                >
-                  {p.name}
-                </Checkbox>
-              </ListItem>
-            ))}
-        </List>
-        {selectedPermissions.length === 0 ? (
-          <></>
-        ) : (
-          <Button isActive onClick={handleAddPermissions} colorScheme={blue}>
-            Add
-          </Button>
-        )}
-      </OverflowYContainer>
-    );
+
+interface Props {
+  assignPermissions?: Permission[];
+}
+
+const PermissionList = ({ assignPermissions }: Props) => {
+  const { data: allPermissions, isLoading, error } = usePermissions();
+  const [selectedPermissions, setSelectedPermissions] = useState<number[]>([]);
+
+  const { id } = useParams();
+  const groupId = parseInt(id!);
+
+  const handleAddPermissions = useAddGroupPermissions(
+    { id: groupId, permission_ids_to_add: selectedPermissions },
+    () => {
+      setSelectedPermissions([]);
+      toast.success("Permissions removed successfully!");
+    }
+  );
+
+  if (error) throw error;
+  if (isLoading) return <Spinner />;
+
+  const handleCheckboxChange = (permissionId: number) => {
+    if (selectedPermissions.includes(permissionId)) {
+      setSelectedPermissions(
+        selectedPermissions.filter((id) => id !== permissionId)
+      );
+    } else {
+      setSelectedPermissions([...selectedPermissions, permissionId]);
+    }
   };
-  
-  export default PermissionList;
-  
+
+  return (
+    <OverflowYContainer>
+      <List>
+        {allPermissions
+          ?.filter((p) => !assignPermissions?.some((ap) => ap.id === p.id))
+          .map((p) => (
+            <ListItem key={p.id}>
+              <Checkbox
+                isChecked={selectedPermissions.includes(p.id)}
+                onChange={() => handleCheckboxChange(p.id)}
+              >
+                {p.name}
+              </Checkbox>
+            </ListItem>
+          ))}
+      </List>
+      {selectedPermissions.length === 0 ? (
+        <Button
+          mt={4}
+          mx={2}
+          colorScheme={blue}
+          // transform="translateX(50%)"
+          w={{ base: "90%", sm: "40%", md: "90%" }}
+          isActive
+          isDisabled
+        >
+          Add
+        </Button>
+      ) : (
+        <Button
+          mt={4}
+          w={{ base: "90%", sm: "40%", md: "90%" }}
+          mx={2}
+          colorScheme={blue}
+          // transform="translateX(50%)"
+          isActive
+          onClick={handleAddPermissions}
+        >
+          Add
+        </Button>
+      )}
+    </OverflowYContainer>
+  );
+};
+
+export default PermissionList;
