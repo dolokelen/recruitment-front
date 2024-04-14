1559e41a429e044631441a28dcc2b37c70c7cdea
dolokelen <dolokelen1@gmail.com>
2024-04-14 19:21:28 +0800

    Safe Mode:

--- src/cacheKeysAndRoutes.ts
+++ src/cacheKeysAndRoutes.ts
@@ -8,7 +8,7 @@ export const AUTH_LAYOUT_ROUTE = "/dashboard";
 export const GROUP_ROUTE = "group-lists";
 export const GROUP_CREATE_ROUTE = "create-group";
 export const GROUP_EDIT_ROUTE = "group-edit";
-export const APPLICATIONDATE_ROUTE = "applicationdate-lists";
+export const APPLICATIONDATE_ROUTE = "applicationdate-lists"; 
 
 export const HOME_ROUTE = "/";
 export const REGISTER_ROUTE = "register";
